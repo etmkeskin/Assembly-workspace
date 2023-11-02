182d531b32b6f0b5b1633670b16d2426953590f6
Etem Keskin <etmkeskin@gmail.com>
2023-11-01 21:53:17 -0400

    labA

--- Lab A/aex4a.asm
+++ Lab A/aex4a.asm
@@ -1,3 +1,12 @@
 lui	x6, (8000 >> 12) + 1
 addi	x6, x6, (8000 & 0xFFF)
 addi	x5, x6, -10
+
+//Another way to solve for better understanding
+//b20:	EQU	8000 >> 12
+//b12:	EQU	8000 & 0xfff
+
+//lui	x6, 2
+//addi	x6, x6, 0xf40
+//addi	x6, x6, -10
+
--- Lab A/aex4b.asm
+++ Lab A/aex4b.asm
@@ -1,3 +1,17 @@
 lui	x6, (-(8000) >> 12)
 addi	x6, x6, (-(8000) & 0xFFF)
 addi	x5, x6, 10
+
+
+//Another way to understand better 
+//b20:	EQU	8000 >> 12
+//b12:	EQU	8000 & 0xfff
+
+//lui	x6, 2		//adding 8000(upper 20bits) to the register 
+//addi	x6, x6, 0xf40		//ading remaning (12bits) to register 
+//xori	x6, x6, -1		//calculating its negative value with 2's complement 
+//addi	x6, x6, 1		//after taking 1's complement add 1 to complete the 2's complement
+//addi	x5, x6, 10
+
+
+
